289e44e3000ddd1d30f0457e8cdf65f772d6cc08
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-11 20:44:22 +0200

    add: comments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import slave
 
 slave.aggiuntaBarcode()
+
+
 slave.creaFile()
--- slave.py
+++ slave.py
@@ -1,7 +1,5 @@
 listaBarcode = [] #lista in cui inserire i barcode da input
-
 dbList=[] #lista per database barcode
-
 
 #importa un csv con elenco di barcode
 
@@ -18,11 +16,10 @@ with  open("csv/db-barcode.csv", mode='r') as dbBarcode:
             continue
         dbList.append(riga[0])
 
-# print(dbList)
-
 def aggiuntaBarcode():
-    #inserire if che verifica che il barcode inserito sia nel csv importato. se true appende barcode alla lista, se false restiuisce messaggio di errore
-    bcode = (input('inserisci barcode\n')) #aggiunge barcode da input. inserire in funzione richiamata ad ogni inserimento
+    #prende in input il barcode
+    bcode = (input('inserisci barcode\n'))
+    #verifica che il barcode inserito sia nel csv importato. se true appende barcode alla lista, se false restiuisce messaggio di errore
     if bcode in dbList:
         listaBarcode.append(bcode)
     else:
@@ -30,17 +27,16 @@ def aggiuntaBarcode():
 
 
 def creaFile():
-    if listaBarcode:
+    if listaBarcode: #verifica che listaBarcode sia piena (True)
         nomeFile= 'output file/' + input('come vuoi chiamare il file?\n') + '.txt' #richiede il nome file all'utente
         file=open(nomeFile,'w') #crea il file con il nome scelto
         
-        #scrive tutti i barcode salvati in lista barcode nel nuovo file
+        #scrive tutti i barcode salvati in listaBarcode nel nuovo file
         for barcode in listaBarcode:
             file=open(nomeFile, 'a')
             file.write(barcode)
             file.write('\n') 
         file.close()
-        #stampa messaggio di avvenuta scrittura del file
-        print('file ' + nomeFile + ' creato')
+        print('file ' + nomeFile + ' creato') #stampa messaggio di avvenuta scrittura del file
     else:
-        print('nessun barcode da salvare')
+        print('nessun barcode da salvare') #stampa errore se listaBarcode è vuota (False)
